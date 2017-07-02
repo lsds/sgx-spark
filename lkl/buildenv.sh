56188438130f53f5e6977be665b5eaf8dcebff9c
Joshua Lind <jdl11@imperial.ac.uk>
2017-07-02 14:49:38 +0100

    Updated Makefile to invoke sgx-lkl using scala libraries and disk image -- not working. Still unable to find SgxMain after exploding

--- lkl/buildenv.sh
+++ lkl/buildenv.sh
@@ -13,3 +13,16 @@ apk add iputils iproute2 unzip libstdc++
 unzip ${jhome}/lib/rt.jar -d /tmp/exploded-rt-jar
 rm ${jhome}/lib/rt.jar
 mv /tmp/exploded-rt-jar ${jhome}/lib/rt.jar
+
+## Trying to unzip jars to class files -- remove all of the below when jar loading works
+# Unzip Spark Core -- because we don't yet support jars :(
+mkdir -p /spark/assembly/target/scala-2.11/jars/spark-core
+unzip /spark/assembly/target/scala-2.11/jars/spark-core_2.11-2.3.0-SNAPSHOT.jar -d /spark/assembly/target/scala-2.11/jars/spark-core
+
+# Unzip Hadoop Common -- because we don't yet support jars :(
+mkdir -p /spark/assembly/target/scala-2.11/jars/hadoop-common
+unzip /spark/assembly/target/scala-2.11/jars/hadoop-common-2.6.5.jar -d /spark/assembly/target/scala-2.11/jars/hadoop-common
+
+# Move all classes to the jar directory
+find /spark/assembly/target/scala-2.11/jars "*.class" -type f -exec cp {} /spark/assembly/target/scala-2.11/jars \; | true
+
