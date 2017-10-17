0e4cc8445e515ce4c1dc614131cc468073846539
Florian Kelbert <fkelbert@acm.org>
2017-10-17 17:44:05 +0100

    refactor

--- core/src/main/scala/org/apache/spark/sgx/SgxFactory.scala
+++ core/src/main/scala/org/apache/spark/sgx/SgxFactory.scala
@@ -1,10 +1,21 @@
 package org.apache.spark.sgx
 
+import java.net.Socket
+
 import org.apache.spark.sgx.iterator.SgxIteratorProvider
 import org.apache.spark.sgx.iterator.socket.SgxSocketIteratorProvider
+import org.apache.spark.sgx.sockets.SocketHelper
 
 object SgxFactory {
 	def newSgxIteratorProvider[T](delegate: Iterator[T], inEnclave: Boolean): SgxIteratorProvider[T] = {
 		return new SgxSocketIteratorProvider[T](delegate, inEnclave);
+	}
+
+	def newSgxCommunicationInterface(): SgxCommunicationInterface = {
+		if (SgxSettings.SGX_USE_SHMEM) ShmCommunicationManager.get().newShmCommunicator()
+		else {
+			if (SgxSettings.IS_ENCLAVE) new SocketHelper(new Socket(SgxSettings.HOST_IP, SgxSettings.HOST_PORT))
+			else new SocketHelper(new Socket(SgxSettings.ENCLAVE_IP, SgxSettings.ENCLAVE_PORT))
+		}
 	}
 }
