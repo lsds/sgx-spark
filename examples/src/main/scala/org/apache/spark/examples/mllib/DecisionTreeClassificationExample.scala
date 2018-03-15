7c7a74bb682a672185deaae9909d485930d8f2b0
anguswilliams91 <anguswilliams91@gmail.com>
2018-03-15 16:54:01 +0000

    Add try/catch block

--- examples/src/main/scala/org/apache/spark/examples/mllib/DecisionTreeClassificationExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/DecisionTreeClassificationExample.scala
@@ -30,46 +30,52 @@ import org.apache.spark.mllib.util.MLUtils
 object DecisionTreeClassificationExample extends Logging {
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("DecisionTreeClassificationExample")
-    val sc = new SparkContext(conf)
+    try {
+      val conf = new SparkConf().setAppName("DecisionTreeClassificationExample")
+      val sc = new SparkContext(conf)
 
-    // $example on$
-    // Load and parse the data file.
-    // val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
-    val data = MLUtils.loadLibSVMFile(sc, args(0))
-    logDebug("loaded data")
-    // Split the data into training and test sets (30% held out for testing)
-    val splits = data.randomSplit(Array(0.7, 0.3))
-    logDebug("split data into train and test sets")
-    val (trainingData, testData) = (splits(0), splits(1))
+      // $example on$
+      // Load and parse the data file.
+      // val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
+      val data = MLUtils.loadLibSVMFile(sc, args(0))
+      logDebug("loaded data")
+      // Split the data into training and test sets (30% held out for testing)
+      val splits = data.randomSplit(Array(0.7, 0.3))
+      logDebug("split data into train and test sets")
+      val (trainingData, testData) = (splits(0), splits(1))
 
-    // Train a DecisionTree model.
-    //  Empty categoricalFeaturesInfo indicates all features are continuous.
-    val numClasses = 2
-    val categoricalFeaturesInfo = Map[Int, Int]()
-    val impurity = "gini"
-    val maxDepth = 5
-    val maxBins = 32
+      // Train a DecisionTree model.
+      //  Empty categoricalFeaturesInfo indicates all features are continuous.
+      val numClasses = 2
+      val categoricalFeaturesInfo = Map[Int, Int]()
+      val impurity = "gini"
+      val maxDepth = 5
+      val maxBins = 32
 
-    val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-      impurity, maxDepth, maxBins)
+      val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+        impurity, maxDepth, maxBins)
 
-    // Evaluate model on test instances and compute test error
-    val labelAndPreds = testData.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
+      // Evaluate model on test instances and compute test error
+      val labelAndPreds = testData.map { point =>
+        val prediction = model.predict(point.features)
+        (point.label, prediction)
+      }
+      val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
+      logDebug("decision tree done")
+      println(s"Test Error = $testErr")
+      println(s"Learned classification tree model:\n ${model.toDebugString}")
+
+      // Save and load model
+      model.save(sc, args(1))
+      // val sameModel = DecisionTreeModel.load(sc, "target/tmp/myDecisionTreeClassificationModel")
+      // $example off$
+
+      sc.stop()
     }
-    val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
-    logDebug("decision tree done")
-    println(s"Test Error = $testErr")
-    println(s"Learned classification tree model:\n ${model.toDebugString}")
-
-    // Save and load model
-    model.save(sc, args(1))
-    // val sameModel = DecisionTreeModel.load(sc, "target/tmp/myDecisionTreeClassificationModel")
-    // $example off$
-
-    sc.stop()
+    catch {
+      case e => logDebug(e.getMessage)
+    }
   }
 }
+
 // scalastyle:on println
