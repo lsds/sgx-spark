b9081dbb42101fcbe5fe8dca788c07294ab97595
Florian Kelbert <fkelbert@acm.org>
2018-04-10 12:32:12 +0100

    Alpine 3.7.0

--- lkl/buildenv.sh
+++ lkl/buildenv.sh
@@ -6,7 +6,7 @@ PATH=/usr/sbin:/sbin:/usr/bin:/bin
 
 cat /etc/apk/repositories
 
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.6/community" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.7/community" >> /etc/apk/repositories
 
 apk update
 apk add iputils iproute2 unzip libstdc++ gcc musl-dev
